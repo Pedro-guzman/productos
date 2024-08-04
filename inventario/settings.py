c61b92d1e3d16f0117620425f1ae9d89b018de7d
pedro guzman carlos <pedroguzmancarlos@iMac-de-pedro.local>
2024-08-04 14:08:51 -0600

    actualización

--- inventario/settings.py
+++ inventario/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Tu_base_de_datos",
-        "USER": "tu_usuario",
-        "PASSWORD": "tu_password",
+        "NAME": "productos",
+        "USER": "postgres",
+        "PASSWORD": "1234",
         "HOST": "localhost",
         "PORT": "5432",
     }
